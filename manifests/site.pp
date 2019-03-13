7a7313dfd3a4e609eda4a8e13efb8d7d4dfb1dcb
piyushjaiswalmonty <piyush.jaiswal@montyholding.com>
2019-03-13 11:35:26 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,17 @@
 node default {
-}
 
-node 'india_smsfw_stp01' {
+}
+node 'master.puppet.vm' {
   include role::master_server
-  file { '/root/README':
-    ensure ==> file,
-    content ==> $fqdn,
+  file {'/root/README':
+    ensure => file,
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
   }
 }
-
-node /^web/ {
+node /^web/ { 
   include role::app_server
 }
-
 node /^db/ {
   include role::db_server
 }
