0c3a79b88b4f92e173db3af6b3183f355a1d7cbb
piyushjaiswalmonty <piyush.jaiswal@montyholding.com>
2019-03-13 11:51:23 +0530

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDbLDvB7iQVT3/vjEwIjWLvcK0jUirmAUgCdMihmnuYZxCFtFcQCTUTpQCcYJeMatctAhEQZ4Z6NgUGCB6TIOpFQYOBBtdMNBf4kdIN1QpXdufMOsk9Mvrfn7pxNFOqnD2YizGAeD0YwliL/5qQxWKKd0jDagEE9fhEPAtzFN86y3qwKqgfqFy7n1Gv5a24nhtcmcobXPG/GUvBkZn2IdTExArIMT0VWQqxp2Yu7BmooUP8rqWt8NhXQvuBY4khkCJ6qFmoPGRbLriqMuSX4mWA/OVCi6/inDdGRGw3QHVZbw6uVQidoZkWZ/+ULGi9NK/14WbQ3YGfQTl5kN0auy8P',
 	}  
 }
